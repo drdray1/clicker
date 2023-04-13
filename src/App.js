725f129d0103371fb3690efb3b7a43bff145b10c
drdray1 <draytonlav@gmail.com>
2023-04-13 01:34:27 -0600

    Clicker working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-3xl font-bold'>Click it</h1>
+      {/* use tailwind to style the button */}
+
+      <MyButton className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' count={count} onClick={handleClick}/>
+      {/* <MyButton count={count} onClick={handleClick} /> */}
+      <MyResetButton onClick={handleReset} />
     </div>
   );
 }
 
-export default App;
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick} type="button" class="animate-bounce inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400" id="mobile-menu-button" aria-expanded="false" aria-haspopup="true">
+      Clicked {count} times
+    </button>
+  );
+}
+
+function MyResetButton({ onClick }) {
+  return (
+    <button onClick={onClick} type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400" id="mobile-menu-button" aria-expanded="false" aria-haspopup="true">
+      Reset Counter
+    </button>
+  );
+}
